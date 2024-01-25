07489dc5ee4a3cc8cffa75265031aa9dea8f8cdd
Erwan-Matthieu <52382415+Erwan-Matthieu@users.noreply.github.com>
2024-01-25 15:54:26 +0300

    Stat sql

---
+++ database/stats.sql
@@ -0,0 +1,61 @@
+CREATE OR REPLACE SEQUENCE genre_seq;
+CREATE OR REPLACE TABLE genres (
+    id_genre VARCHAR(10) PRIMARY KEY,
+    nom VARCHAR(10)
+);
+CREATE OR REPLACE SEQUENCE client_seq;
+CREATE TABLE client (
+    id_client VARCHAR(10) PRIMARY KEY,
+    nom VARCHAR(255),
+    id_genre VARCHAR(10) REFERENCES genres(id_genre)
+);
+CREATE OR REPLACE SEQUENCE achat_seq;
+CREATE TABLE achats(
+    id_achat VARCHAR(10) PRIMARY KEY,
+    id_client VARCHAR(10) REFERENCES client(id_client),
+    id_produit VARCHAR(10) REFERENCES produit(id_produit),
+    id_format VARCHAR(10) REFERENCES format(id_format),
+    date DATE,
+    quantite INT
+);
+ALTER TABLE achats ADD COLUMN id_format VARCHAR(10) REFERENCES format(id_format);
+CREATE OR REPLACE VIEW v_initial AS
+SELECT
+    p.id_produit,
+    f.id_format,
+    g.id_genre,
+    0 AS nombre
+FROM
+    produit p, format f,genres g;
+CREATE OR REPLACE VIEW v_stat_produit_format_genre AS
+SELECT
+    c.id_genre,
+    a.id_produit,
+    a.id_format,
+    SUM(a.quantite) AS nombre
+FROM
+    achats a
+JOIN
+    client c
+ON
+    a.id_client = c.id_client
+GROUP BY
+    c.id_genre,
+    a.id_produit,
+    a.id_format;
+CREATE OR REPLACE VIEW v_stat_initiale AS
+SELECT
+    vi.id_produit,
+    vi.id_format,
+    vi.id_genre,
+    vi.nombre
+FROM    
+    v_initial vi
+UNION ALL
+SELECT
+    vspfg.id_produit,
+    vspfg.id_format,
+    vspfg.id_genre,
+    vspfg.nombre 
+FROM
+    v_stat_produit_format_genre vspfg;
